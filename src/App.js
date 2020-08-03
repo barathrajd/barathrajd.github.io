3a0348e8d668f85de7e6a174eb6cf301f7161241
barathrajd <barathdeva757@gmail.com>
2020-08-03 09:55:54 +0530

    Ui Updated

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
         <Navbar />
         <Switch>
           <Route exact path='/' component={Home} />
-          <Route path='/about' component={About} />
+          <Route exact path='/about' component={About} />
         </Switch>
       </Router>
     </div>
